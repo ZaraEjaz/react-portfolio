2f64204c148075058c8e9ce8db9b363f441ad78a
Zara Ejaz <zaraejaz102003@gmail.com>
2024-08-19 20:07:41 +0500

    Commit

--- vite-project/src/Components/Footer/Footer.jsx
+++ vite-project/src/Components/Footer/Footer.jsx
@@ -5,50 +5,40 @@ import Media from '../Media/Media';
 import emailjs from 'emailjs-com';
 
 const Footer = () => {
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then((result) => {
-        console.log(result.text);
-        alert('Message sent successfully!');
-      }, (error) => {
-        console.log(error.text);
-        alert('Failed to send message. Please try again.');
-      });
-  };
-
   return (
     <>
-      <div className="container">
-        <footer id="footer" className="py-5">
-          <div className="container">
-            <div className="row container">
-              {/* Column 1: Media Component */}
-              <div className="col-md-1 mb-2">
-                
-              </div>
-              {/* Column 2: Contact Form */}
-              
-
-              {/* Column 3: Location, Email, Phone */}
-              <div className="col-md-4 mb-4 ml-5">
-                <div className="contact-info">
-                  <h4>Contact Information</h4><br/>
-               
-                  <p4><strong>Location:</strong>  &nbsp;Sabir SRE Defence Phase 2 Karachi</p4>
-                  
-                  <p4><strong>Email:</strong> &nbsp; zaraejaz102003@gmail.com</p4>
-                  <p4><strong>Phone:</strong> &nbsp;+92 327 2047558</p4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+    <div className='footer-container'>
+    <div className="getintouch-heading">
+            <article>Get in Touch();</article>
       </div>
-              
-    </>
-  );
-};
+      <div className="logos">
+            <a href="https://www.instagram.com/zaraughhh" target="_blank" aria-label="My Instagram">
+              <svg viewBox="0 0 448 512" className="social-handle">
+                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9 114.9-51.3 114.9-114.9S287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8s-26.8-12-26.8-26.8 12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+              </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/zara-ejaz-431aa2273?original_referer=" target="_blank" aria-label="My LinkedIn">
+              <svg className="social-handle" viewBox="0 0 448 512">
+                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+              </svg>
+            </a>
+            <a href="https://github.com/ZaraEjaz" target="_blank" aria-label="My GitHub">
+              <svg viewBox="0 0 496 512" className="social-handle">
+                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8z"/>
+              </svg>
+</a>
+</div>
 
+    </div>
+  
+      <footer id="footer">
+        <div className="footer-container">
+<div className="end-strip">
+<p>© 2023 - All Rights Reserved.</p>
+</div>
+</div>
+</footer>
+</>
+);
+};
 export default Footer;
