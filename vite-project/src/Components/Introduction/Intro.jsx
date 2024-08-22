9c645b1e6c13ba7cce6fb14b6202ae2c7ef6d001
Zara Ejaz <zaraejaz102003@gmail.com>
2024-08-22 16:29:46 +0500

    Responsive

--- vite-project/src/Components/Introduction/Intro.jsx
+++ vite-project/src/Components/Introduction/Intro.jsx
@@ -11,7 +11,7 @@ const Intro = () => {
       <div className="row align-items-center">
         <div className="col-lg-7 col-md-6 col-sm-12 intro-text">
           <p className="tracking-in-expand-fwd-bottom mb-5">About me .____________________</p>
-          <h1 className="tracking-in-expand-fwd-bottom mb-5">Zara Ejaz</h1>
+          <h1 className="tracking-in-expand-fwd-bottom ">Zara Ejaz</h1>
           <p className="intro-paragraph tracking-in-expand-fwd-bottom">
           Hello, I'm Zara Ejaz — a passionate software engineering student with a flair for web design and development. My journey in the tech world is fueled by a curiosity to explore the endless possibilities of code and a drive to create seamless, user-friendly digital experiences. 
 <br /><br />
--- vite-project/src/Components/Section1/Section1.jsx
+++ vite-project/src/Components/Section1/Section1.jsx
@@ -41,7 +41,7 @@ const Section1 = () => {
         <p className={`tracking-in-expand-fwd-bottom ${isVisible ? 'animate' : ''}`}>Hello 👋 I'm</p>
         <h1 className={`name tracking-in-expand-fwd-bottom ${isVisible ? 'animate' : ''}`}>Zara Ejaz.</h1>
         <p className={`work tracking-in-expand-fwd-bottom ${isVisible ? 'animate' : ''}`}>I'm a Web Developer.</p>
-      </div>
+      </div><br/>
       <p className={`info-para mb-5 ${isVisible ? 'animate' : ''}`} data-aos="fade-up" data-aos-delay="600">
         As a web developer with expertise in frontend web design, I have a passion for creating engaging web interfaces, collaborating with others, and developing innovative products. <br /><br />
         I prioritize a straightforward content structure, elegant design patterns, and meaningful interactions.
