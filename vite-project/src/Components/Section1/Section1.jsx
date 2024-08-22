7a96f217916cffd2c8a241b7ef0737de8571a408
Zara Ejaz <zaraejaz102003@gmail.com>
2024-08-22 16:24:36 +0500

    Responsive

--- vite-project/src/Components/Section1/Section1.jsx
+++ vite-project/src/Components/Section1/Section1.jsx
@@ -42,7 +42,7 @@ const Section1 = () => {
         <h1 className={`name tracking-in-expand-fwd-bottom ${isVisible ? 'animate' : ''}`}>Zara Ejaz.</h1>
         <p className={`work tracking-in-expand-fwd-bottom ${isVisible ? 'animate' : ''}`}>I'm a Web Developer.</p>
       </div>
-      <p className={`info-para mt-5 mb-5 ${isVisible ? 'animate' : ''}`} data-aos="fade-up" data-aos-delay="600">
+      <p className={`info-para mb-5 ${isVisible ? 'animate' : ''}`} data-aos="fade-up" data-aos-delay="600">
         As a web developer with expertise in frontend web design, I have a passion for creating engaging web interfaces, collaborating with others, and developing innovative products. <br /><br />
         I prioritize a straightforward content structure, elegant design patterns, and meaningful interactions.
       </p>
